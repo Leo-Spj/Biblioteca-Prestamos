a1cbebcf32302f295b68e4fb30edbd9c7b0fc708
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-30 18:53:39 -0500

    test de conexion

--- src/test/java/dao/UsuarioDAOTest.java
+++ src/test/java/dao/UsuarioDAOTest.java
@@ -1,2 +1,31 @@
-package dao;public class UsuarioDAOTest {
+package dao;
+
+import com.utp.biblioteca.resources.dao.UsuarioDAO;
+import com.utp.biblioteca.resources.modelo.Usuario;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsuarioDAOTest {
+
+    @Test
+    public void testBuscarTodos() {
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        List<Usuario> usuarios = usuarioDAO.buscarTodos();
+
+        // Verificar que la lista no está vacía
+        assertFalse(usuarios.isEmpty());
+
+        // Verificar que todos los elementos en la lista son instancias de Usuario
+        for (Object obj : usuarios) {
+            assertTrue(obj instanceof Usuario);
+        }
+
+        // Imprimir los usuarios
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+    }
 }
