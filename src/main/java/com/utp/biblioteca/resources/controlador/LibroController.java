32ea717a0bfa46cde45ef904227d82d37e0c46ee
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-30 17:28:16 -0500

    controladores bd

---
+++ src/main/java/com/utp/biblioteca/resources/controlador/LibroController.java
@@ -0,0 +1,6 @@
+package com.utp.biblioteca.resources.controlador;
+
+public class LibroController {
+
+
+}
---
+++ src/main/java/com/utp/biblioteca/resources/controlador/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.utp.biblioteca.resources.controlador;
+
+public class UsuarioController {
+}
