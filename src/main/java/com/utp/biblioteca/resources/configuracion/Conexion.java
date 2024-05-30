bfa8fdc401a336eefe44a26dc8814212dc7d5ef5
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-30 14:43:16 -0500

    conexion a bd

--- src/main/java/com/utp/biblioteca/resources/configuracion/Conexion.java
+++ src/main/java/com/utp/biblioteca/resources/configuracion/Conexion.java
@@ -1,2 +1,21 @@
-package com.utp.biblioteca.resources.configuracion;public class Conexion {
+package com.utp.biblioteca.resources.configuracion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private Connection conn;
+    DatabaseConfig databaseConfig = new DatabaseConfig();
+
+    public Connection getConectar() throws SQLException {
+        String url = databaseConfig.getUrl();
+        String username = databaseConfig.getUsername();
+        String password = databaseConfig.getPassword();
+
+        conn = DriverManager.getConnection(url, username, password);
+
+        return conn;
+    }
 }
--- src/main/java/com/utp/biblioteca/resources/configuracion/DatabaseConfig.java
+++ src/main/java/com/utp/biblioteca/resources/configuracion/DatabaseConfig.java
@@ -1,2 +1,32 @@
-package com.utp.biblioteca.resources.configuracion;public class DatabaseConfig {
+package com.utp.biblioteca.resources.configuracion;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DatabaseConfig {
+
+    private Properties properties;
+
+    public DatabaseConfig() {
+        properties = new Properties();
+
+        try ( FileInputStream fis = new FileInputStream("src/main/resources/db/db.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUrl() {
+        return properties.getProperty("db.url");
+    }
+
+    public String getUsername() {
+        return properties.getProperty("db.username");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("db.password");
+    }
 }
