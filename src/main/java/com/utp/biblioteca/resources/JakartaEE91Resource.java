88c6e2fa0664d3cfb37ff9d347d5599fca598557
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-06-05 19:18:55 -0500

    glassfish 6.2.5, jakarta 9.1, jdk 17

--- src/main/java/com/utp/biblioteca/resources/JakartaEE91Resource.java
+++ src/main/java/com/utp/biblioteca/resources/JakartaEE91Resource.java
@@ -8,8 +8,8 @@ import jakarta.ws.rs.core.Response;
  *
  * @author 
  */
-@Path("jakartaee10")
-public class JakartaEE10Resource {
+@Path("jakartaee9")
+public class JakartaEE91Resource {
     
     @GET
     public Response ping(){
