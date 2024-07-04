85ee260e85ba9232c126f9b8863eea1e6bb5970d
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-03 21:09:29 -0500

    Formulario de Usuario habilitado y cambios menores extras

---
+++ src/main/java/com/utp/biblioteca/resources/controlador/beans/UsuarioBean.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Beans/Bean.java to edit this template
+ */
+package com.utp.biblioteca.resources.controlador.beans;
+
+import com.utp.biblioteca.resources.modelo.Rol;
+import com.utp.biblioteca.resources.modelo.Usuario;
+import com.utp.biblioteca.resources.modelo.dao.RolDao;
+import com.utp.biblioteca.resources.modelo.dao.UsuarioDao;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.beans.*;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author brandonluismenesessolorzano
+ */
+@Named
+@ViewScoped
+public class UsuarioBean implements Serializable {
+
+    private UsuarioDao usuarioDao = new UsuarioDao();
+    private Usuario usuario = new Usuario();
+    private RolDao rolDao = new RolDao();
+    private int rolId;
+
+    public UsuarioBean() {
+    }
+
+    public List<Rol> getRoles() {
+        return rolDao.buscarTodos();
+    }
+
+    public int getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(int rolId) {
+        this.rolId = rolId;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void submit() {
+        Rol rolCompleto = rolDao.buscarUno(rolId);
+
+        usuario.setRol(rolCompleto);
+
+        System.out.println("Usuario: " + usuario);
+        usuarioDao.crear(usuario);
+    }
+}
